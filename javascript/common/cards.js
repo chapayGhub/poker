2d4e403ac1a0023d96e05a61a1f5a65084e55eeb
kkjk21 <kkjk21@gmail.com>
2013-09-02 17:32:04 -0700

    Replaced 'number' by 'rank'.

--- javascript/common/cards.js
+++ javascript/common/cards.js
@@ -2,13 +2,13 @@
  * @fileoverview Utility functions for cards representation.
  * We represent cards by both string and number.
  *
- * By string each card is represented by exact 2 characters; suit and number.
+ * By string each card is represented by exact 2 characters; suit and rank.
  * Each suit Spade, Heart, Diamond and Club is represented by
- * 's', 'h', 'd' and 'c' respectively, and the number of cards is represented as
+ * 's', 'h', 'd' and 'c' respectively, and the rank of cards is represented as
  * follows; for 2 to 9 they are represented by the character as it is,
  * say Duce is '2', for 10, Jack, Queen, King and Ace they are represented by
  * 'T', 'J', 'Q', 'K' and 'A' respectively. Then each card is represented by the
- * combination of the suit and number of the card like below.
+ * combination of the suit and rank of the card like below.
  * Duce of Spade: 's2'
  * Queen of Club: 'cQ'
  * As for a pair of cards we just join each card of string.
@@ -16,9 +16,9 @@
  * By number each card is represented by 1 to 52 since the number of cards is
  * 52. 1 represents Two of Spade, 2 is Two of Heart, 3 is Two of Diamond, 4 is
  * Two of Club, 5 is Three of Spade, and consequently 52 is Ace of Club.
- * In other words, reminder for the number divided by 4 represents the suite and
- * quotient does the number for cards. The reason why Two is the starting number
- * is Two is the weakest number for poker and thus for ease of development in
+ * In other words, reminder for the number divided by 4 represents the suit and
+ * quotient does the rank for cards. The reason why Two is the starting rank
+ * is Two is the weakest rank for poker and thus for ease of development in
  * terms of comparison or sorting.
  * 0 exists for a special number that represents no card so that we can
  * represent multiple number of cards (i.e., if 0 is used for some card it is
@@ -45,10 +45,10 @@ poker.cards.Suit = {
 
 
 /**
- * Card number representations for 10 to A.
+ * Card rank representations for 10 to A.
  * @const {Object.<string>}
  */
-poker.cards.Numbers = {
+poker.cards.Ranks = {
   '10': 'T',
   '11': 'J',
   '12': 'Q',
@@ -94,7 +94,7 @@ poker.cards.toString = function(cardsNum) {
  * @private
  */
 poker.cards.fromStringSingle_ = function(cardStr) {
-  var suit, number;
+  var suit, rank;
   // Gets suit.
   switch (cardStr[0]) {
     case poker.cards.Suit.SPADE: suit = 0; break;
@@ -103,20 +103,20 @@ poker.cards.fromStringSingle_ = function(cardStr) {
     case poker.cards.Suit.CLUB: suit = 3; break;
     default: goog.asserts.fail('Unknown suit: ' + cardStr[0]);
   }
-  // Gets number.
+  // Gets rank.
   if (['2', '3', '4', '5', '6', '7', '8', '9'].contains(cardStr[1])) {
-    number = parseInt(cardStr[1]) - 2;
+    rank = parseInt(cardStr[1]) - 2;
   } else {
     switch (cardStr[1]) {
-      case poker.cards.Numbers['10']: number = 8; break;
-      case poker.cards.Numbers['11']: number = 9; break;
-      case poker.cards.Numbers['12']: number = 10; break;
-      case poker.cards.Numbers['13']: number = 11; break;
-      case poker.cards.Numbers['1']: number = 12; break;
-      default: goog.asserts.fail('Wrong number: ' + cardStr[1]);
+      case poker.cards.Ranks['10']: rank = 8; break;
+      case poker.cards.Ranks['11']: rank = 9; break;
+      case poker.cards.Ranks['12']: rank = 10; break;
+      case poker.cards.Ranks['13']: rank = 11; break;
+      case poker.cards.Ranks['1']: rank = 12; break;
+      default: goog.asserts.fail('Wrong rank: ' + cardStr[1]);
     }
   }
-  return suit + 4 * number + 1;
+  return suit + 4 * rank + 1;
 };
 
 
@@ -127,7 +127,7 @@ poker.cards.fromStringSingle_ = function(cardStr) {
  * @private
  */
 poker.cards.toStringSingle_ = function(cardNum) {
-  var suit, number;
+  var suit, rank;
   // Gets suit.
   switch ((cardNum - 1) % 4) {
     case 0: suit = poker.cards.Suit.SPADE; break;
@@ -135,16 +135,16 @@ poker.cards.toStringSingle_ = function(cardNum) {
     case 2: suit = poker.cards.Suit.DIAMOND; break;
     case 3: suit = poker.cards.Suit.CLUB; break;
   }
-  // Gets number.
+  // Gets rank.
   var numNum = (cardNum - 1) / 4 + 2;
   if (numNum == 14) {
     numNum = 1;
   }
   if (numNum >= 2 && numNum <= 9) {
-    number = numNum.toString();
+    rank = numNum.toString();
   } else {
-    number = poker.cards.Numbers[numNum.toString()];
-    goog.asserts.assert(number, 'Wrong number: ' + numNum.toString());
+    rank = poker.cards.Ranks[numNum.toString()];
+    goog.asserts.assert(rank, 'Wrong rank: ' + numNum.toString());
   }
-  return suit + number;
+  return suit + rank;
 };
